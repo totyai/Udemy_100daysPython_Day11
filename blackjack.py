8a860c1f29acf5e71e561440ca2397c6d54d653b
totyai <90376621+totyai@users.noreply.github.com>
2025-11-16 19:44:06 +0000

    - Finished blackjack project - Implemented to Notes

--- blackjack.py
+++ blackjack.py
@@ -1,5 +1,6 @@
 import random
 
+# Global variables
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 logo =  """
 .------.            _     _            _    _            _    
@@ -11,31 +12,103 @@ logo =  """
       |  \\/ K|                            _/ |                
       '------'                           |__/           
 """
+player_hand = {}
+computer_hand = {}
+cont = True
+def init():
+    global player_hand, computer_hand, cont
+    player_hand = {
+        "hand": [],
+        "sum": 0 
+    }
+    computer_hand = {
+        "hand": [],
+        "sum": 0
+    }
+    cont = True
 
-play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
-player_hand = {
-    "hand": [],
-    "sum": 0 
-}
-computer_hand = {
-    "hand": [],
-    "sum": 0
-}
+def hand_com(round):
+    global player_hand, computer_hand
+    print(f"Your {round} hand: {player_hand['hand']}, Total: {player_hand['sum']}.")
+    print(f"Opponent's {round} hand: {computer_hand['hand']}, Total: {computer_hand['sum']}")
 
-player_hand["hand"].append(random.choice(cards))
-player_hand["hand"].append(random.choice(cards))
-computer_hand["hand"].append(random.choice(cards))
-player_hand["sum"] = sum(player_hand["hand"])
-computer_hand["sum"] = sum(computer_hand["hand"])
+def add_card(deck):
+    deck["hand"].append(random.choice(cards))
+    deck["sum"] = sum(deck["hand"])
 
-# TODO - Ask to add more card to user
+def game():
+    global player_hand, computer_hand, cont
+    print(logo)
 
-# TODO - Decide winner by compare
+    for i in range(2):
+        add_card(player_hand)
+    add_card(computer_hand)
 
-# TODO - Lose if hand is more then 21
+    hand_com("current")
 
-# TODO - User communications
+    if player_hand["sum"] == 21:
+        print("You win with Blackjack 😃")
+        init()
+        return
 
-# TODO - 
 
-print(player_hand, computer_hand)
+    # TODO - Ask to add more card to user
+    while cont:
+        more = input("Do you want 1 more card? Type 'y' for yes, 'n' for no: ").lower()
+        if more == "y":
+            add_card(player_hand)
+            if player_hand["sum"] > 21:
+                if 11 in player_hand["hand"]:
+                    player_hand["hand"].remove(11)
+                    player_hand["hand"].append(1)
+                    player_hand["sum"] = sum(player_hand["hand"])
+                    cont = True
+                else:
+                    cont = False
+                if player_hand["sum"] > 21:
+                    print(f"Your new hand: {player_hand['hand']}, Total: {player_hand['sum']}")
+                    print("You went over. You lose 😭")
+                    init()
+                    return
+            hand_com("current")
+        elif more == "n":
+            cont = False
+        else:
+            print("Invalid input, please use 'y' or 'n'. ")
+
+    while computer_hand["sum"] < 17:
+        add_card(computer_hand)
+
+    # TODO - Player sum not more 21
+    if computer_hand["sum"] > 21:
+        hand_com("final")
+        print("Opponent went over. You win. 😃")
+    # TODO - Decide winner by compare
+    elif computer_hand["sum"] > player_hand["sum"]:
+        hand_com("final")
+        print("You lose 😤")
+    elif computer_hand["sum"] == player_hand["sum"]:
+        hand_com("final")
+        print("Draw 🙃")
+    elif computer_hand["sum"] < player_hand["sum"]:
+        hand_com("final")
+        print("You win 😃")
+    
+    init()
+
+def main():
+    play = True
+    while play:
+        play = input("Do you want to play Blackjack? Type 'y' or 'n': ").lower()
+        if play == "y":
+            init()
+            play = True
+            game()
+        elif play == "n":
+            play = False
+            print("The program exits. Thank you.")
+        else:
+            print("Wrong input. Try again.")
+
+if __name__ == "__main__":
+    main()
